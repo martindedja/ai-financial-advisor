5ad7659c7a458d6576291e98ecf101f4d1b83394
Martin Dedja <martindedja11@gmail.com>
2024-01-04 15:37:11 -0600

    Re-initialized repo

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,7 @@
 <template>
-  {{ message }}
+  <RouterView></RouterView>
 </template>
 
-<script>
-import axios from 'axios';
-
-export default {
-  name: 'App',
-  components: {
-  },
-  data() {
-    return {
-      message: '',
-    }
-  },
-  mounted() {
-    axios.get('http://localhost:3000/api/hello')
-      .then((response) => {
-        this.message = response.data.message;
-      })
-      .catch((error) => {
-        console.error('API request error:', error);
-      });
-  },
-}
+<script setup lang="ts">
+import { RouterView } from "vue-router";
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
